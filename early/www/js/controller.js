6f46402cf4466e60b9722e0694817c89574ac255
tanghj <13817595130@163.com>
2016-08-24 11:07:42 +0800

    addCode

---
+++ early/www/js/controller.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers', ['ionic'])
+
+.controller("myCtrl",function($scope,$state){
+	$state.go("home");
+})
+
+
+// home
+.controller("homeCtrl",function($scope, $state) {
+	
+})
+
+// pageOne
+.controller("pageOneCtrl",function($scope, $state, $stateParams) {
+	
+})
